e021d19dd60ffbb1c1d838b167b3b21f2616fdf0
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-24 15:42:16 +0200

    added some initial data in seeds and set up bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+users = [
+    {
+        :name => 'John',
+        :email => 'john@example.com',
+        :password => 'password'
+    }
+]
+
+users.each do |user|
+    User.create(name: user[:name], email: user[:email], password: user[:password])
+end
+recipes = [
+    {
+       :name => "Pizza",
+       :preparation_time => 0.3,
+       :cooking_time => 1,
+       :public => true,
+       :description => "italian pizza is one of the best recipes which displays the italian heritage",
+       :user_id => 1
+    },
+    {
+       :name => "Rice",
+       :preparation_time => 0.3,
+       :cooking_time => 1.5,
+       :public => true,
+       :description => " This is one of the best recipes which are easy to prepare",
+       :user_id => 1
+    },
+    {
+       :name => "Rice",
+       :preparation_time => 0.3,
+       :cooking_time => 1.5,
+       :public => true,
+       :description => " This is one of the best recipes which are easy to prepare",
+       :user_id => 1
+    }
+]
+
+recipes.each do |recipe|
+    Recipe.create(name: recipe[:name], preparation_time: recipe[:preparation_time],
+         cooking_time: recipe[:cooking_time], public: recipe[:public], description: recipe[:description], user_id: recipe[:user_id])
+end

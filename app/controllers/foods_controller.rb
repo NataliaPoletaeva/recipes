45d47691a3dd4fe0db739f33fb048c36d57a62b3
Natalia Poletaeva <natfly799@gmail.com>
2022-05-24 21:07:47 +0500

    Fix linters

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -10,13 +10,14 @@ class FoodsController < ApplicationController
   def create
     @new_food = current_user.foods.new(food_params)
     if @new_food.save
-      redirect_to foods_path, flash: { alert: 'Your food is saved' } 
+      redirect_to foods_path, flash: { alert: 'Your food is saved' }
     else
       redirect_to new_food_path, flash: { alert: 'Could not save your food' }
     end
   end
 
   private
+
   def food_params
     params.require(:food).permit(:name, :measurement_unit, :price)
   end

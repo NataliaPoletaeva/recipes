88423ea1d799ad6ec24e9da45b2cc25175cefa24
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-24 15:02:07 +0200

    added relationship between user and recipe

--- db/migrate/20220524082425_devise_create_users.rb
+++ db/migrate/20220524082425_devise_create_users.rb
@@ -2,6 +2,7 @@
 
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
+    create_table :users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
@@ -32,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      # t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true

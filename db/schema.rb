44fdb78b67b5492603b0314ce07c535a836f73e5
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-25 13:31:28 +0200

    improvements on views

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_153642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurement_unit"
+    t.float "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
   create_table "recipe_foods", force: :cascade do |t|
     t.bigint "quantity"
     t.datetime "created_at", null: false
@@ -32,16 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_153642) do
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
-  create_table "foods", force: :cascade do |t|
-    t.string "name"
-    t.string "measurement_unit"
-    t.float "price"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,6 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_153642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "recipes", "users"
   add_foreign_key "foods", "users"
+  add_foreign_key "recipes", "users"
 end

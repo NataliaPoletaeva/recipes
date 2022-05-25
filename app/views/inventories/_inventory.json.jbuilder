537085307fe0cb85f8cfca7d38a26efba916d6b9
unknown <butlermuwo@gmail.com>
2022-05-25 22:57:51 +0200

    Add inventory list

---
+++ app/views/inventories/_inventory.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! inventory, :id, :name, :description, :created_at, :updated_at
+json.url inventory_url(inventory, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :inventory_foods
+  resources :inventories
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

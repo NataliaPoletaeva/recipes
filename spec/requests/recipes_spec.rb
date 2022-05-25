6e344ecce1ec50e3d0553a403340063863b59017
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-05-25 12:37:13 +0200

    modified spec file

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,25 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
-  describe 'GET /recipes' do
-    before :each do
-      get recipes_path
-    end
-
-    it 'responds with status 200' do
-      expect(response).to have_http_status(200)
-    end
-
-    it 'renders the recipes/index view' do
-      expect(response).to render_template('recipes/index')
-    end
-
-    context 'with render_views enabled' do
-      it 'renders view with home page text in the index template' do
-        expect(response.body).to include('Recipes')
-      end
-    end
-  end
   describe 'GET /recipes/:id' do
     before :each do
       recipe = FactoryBot.create(:recipe)

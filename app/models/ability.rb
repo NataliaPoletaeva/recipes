d93daad4bd0ba7b5a356bdc67db1fb41034cddfa
Natalia Poletaeva <natfly799@gmail.com>
2022-05-27 12:56:13 +0500

    Fix authorisation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,3 +1,10 @@
 class Ability
   include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    can :manage, Recipe, user_id: user.id
+    can :manage, Food, user_id: user.id
+    can :read, :all
+  end
 end
